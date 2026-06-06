794b73b4c1e496e78c0ea09cc3f253dbe553f772
Vladimir Egorov <mktji.job@gmail.com>
2026-06-06 23:57:28 +0400

    fix(about): delete extra row of fw

--- src/ui/settings/about.cpp
+++ src/ui/settings/about.cpp
@@ -50,9 +50,6 @@ void drawAbout(const AboutView &view) {
   display.setCursor(0,
                     ui::layout::INFO_ROW1_Y + ui::layout::INFO_ROW_STEP_Y * 3);
 
-  display.print("FW: ");
-  display.print(view.firmwareVersion);
-
   char uptimeBuf[16];
   formatUptime(view.uptimeSeconds, uptimeBuf, sizeof(uptimeBuf));
   display.setCursor(0,
